1067bfcdc44d2674936efade1d8711a4e9728b2f
James Turk <james.p.turk@gmail.com>
2011-09-20 16:31:22 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup
+from oyster import __version__
+
+long_description = open('README.rst').read()
+
+setup(name="oyster",
+      version=__version__,
+      py_modules=['oyster'],
+      author="James Turk",
+      author_email='jturk@sunlightfoundation.com',
+      license="BSD",
+      url="http://github.com/sunlightlabs/oyster/",
+      long_description=long_description,
+      description="a library for scraping things",
+      platforms=["any"],
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   ],
+      install_requires=["httplib2 >= 0.6.0", "scrapelib >= 0.5.4",
+                        "pymongo >= 1.11", "flask", "celery"],
+      entry_points="""
+[console_scripts]
+scrapeshell = scrapelib:scrapeshell
+"""
+      )
