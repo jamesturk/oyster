d1df7374747f4fd4e3de941d883d7a331d2f8316
James Turk <james.p.turk@gmail.com>
2011-09-27 17:24:47 -0400

    use MONGO_ settings for broker

--- oyster/celeryconfig.py
+++ oyster/celeryconfig.py
@@ -3,8 +3,10 @@ from oyster.conf import settings
 CELERY_IMPORTS = ("oyster.tasks",)
 
 BROKER_TRANSPORT = 'mongodb'
-CELERY_RESULT_BACKEND = 'mongodb'
+BROKER_HOST = settings.MONGO_HOST
+BROKER_PORT = settings.MONGO_PORT
 
+CELERY_RESULT_BACKEND = 'mongodb'
 CELERY_MONGODB_BACKEND_SETTINGS = {
     'host': settings.MONGO_HOST,
     'port': settings.MONGO_PORT,
